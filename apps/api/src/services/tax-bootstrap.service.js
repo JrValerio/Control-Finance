3407fa7e244ce0a20c84e88c3cd6173bfae49955
JrValerio <amarovsjr81@gmail.com>
2026-03-31 17:41:11 -0300

    fix(tax): unifica contrato anual no bootstrap e summary

--- apps/api/src/services/tax-bootstrap.service.js
+++ apps/api/src/services/tax-bootstrap.service.js
@@ -6,15 +6,18 @@ import {
   TAX_FACT_TYPES,
   TAX_RULE_FAMILIES,
 } from "../domain/tax/tax.constants.js";
+import { listTaxRuleSeedYears } from "../domain/tax/tax-rules.engine.js";
 import { normalizeTaxUserId } from "../domain/tax/tax.validation.js";
 
 export const getTaxBootstrapByUser = async (userId) => {
   normalizeTaxUserId(userId);
+  const supportedTaxYears = listTaxRuleSeedYears();
 
   return {
     module: "tax",
     scope: "irpf_mvp",
     apiVersion: resolveApiVersion(),
+    supportedTaxYears,
     documentTypes: [...TAX_DOCUMENT_TYPES],
     documentProcessingStatuses: [...TAX_DOCUMENT_PROCESSING_STATUSES],
     factTypes: [...TAX_FACT_TYPES],
